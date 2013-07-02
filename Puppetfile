c81a7557bf3157f34ae51fd91805684dfeee0019
root <root@ospoc2.lab.noc.harvard.edu>
2013-07-02 15:53:36 -0400

    a controller node build (WIP)

--- Puppetfile
+++ Puppetfile
@@ -4,9 +4,6 @@
 mod 'r10k',
   :git => 'https://github.com/tfhartmann/puppet-r10k.git'
 
-#mod 'openstack',
-#  :git => 'https://github.com/stackforge/puppet-openstack.git'
-
 # This is currently a noop but will be supported in the future.
 forge 'forge.puppetlabs.com'
 
@@ -18,11 +15,15 @@ mod 'puppetlabs/firewall', '0.3.0'
 mod 'puppetlabs/mysql', '0.6.1'
 mod 'puppetlabs/postgresql', '2.1.1'
 mod 'puppetlabs/rabbitmq'
+mod 'saz/memcached'
 mod 'puppetlabs/puppetdb', '1.2.1'
 mod 'puppetlabs/stdlib', '3.2.0'
 mod 'puppetlabs/vcsrepo', '0.1.1'
 mod 'ripienaar/concat', '0.2.0'
 mod 'thias/glusterfs', '0.0.2'
+mod 'puppetlabs/apache', '0.6.0'
+
+mod 'vswitch', :git => 'https://github.com/bodepd/puppet-vswitch.git'
 
 # Install OpenSTack puppet modules recent v2.0.0 release
 mod 'puppetlabs/keystone', '2.0.0'
---
+++ nodes/ospoc2-controller.pp
@@ -0,0 +1,41 @@
+
+
+#class { 'openstack::db::mysql':
+#  mysql_root_password  => 'changeme',
+#  keystone_db_password => 'changeme',
+#  glance_db_password   => 'changeme',
+#  nova_db_password     => 'changeme',
+#  cinder_db_password   => 'changeme',
+#  quantum_db_password  => 'changeme',
+#  allowed_hosts        => ['10.255.73.%'],
+#}
+
+class { 'openstack::controller':
+    public_address          => $::ipadress_eth10,
+    public_interface        => 'eth10',
+    private_interface       => 'eth10',
+    internal_address        => '192.168.101.10',
+    floating_range          => '192.168.101.64/28',
+    fixed_range             => '10.0.0.0/24',
+    multi_host              => false,
+    network_manager         => 'nova.network.manager.FlatDHCPManager',
+    admin_email             => 'admin@example.com',
+    admin_password          => 'changeme',
+    keystone_admin_token    => 'keystone_admin_token',
+    keystone_db_password    => 'changeme',
+    glance_user_password    => 'changeme',
+    glance_db_password      => 'changeme',
+    cinder_user_password    => 'changeme',
+    cinder_db_password      => 'changeme',
+    nova_user_password      => 'changeme',
+    nova_db_password        => 'changeme',
+    quantum_user_password   => 'changeme',
+    quantum_db_password     => 'changeme',
+    bridge_interface        => 'eth10',
+    rabbit_password         => 'changeme',
+    rabbit_user             => 'rabbit_user',
+    secret_key              => 'secret_key',
+    db_host                 => $::ipadress_eth10,
+    metadata_shared_secret           => 'changeme',
+}
+
